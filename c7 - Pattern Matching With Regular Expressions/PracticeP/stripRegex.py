16be223a9cbcab2bc07b11bfb01b58fc4e3630cd
Flavio <flaviolago@Flavios-Mac-mini.local>
2021-08-13 23:55:43 -0700

    first commit

---
+++ c7 - Pattern Matching With Regular Expressions/PracticeP/stripRegex.py
@@ -0,0 +1,33 @@
+#! Python3
+# stripRegex.py
+# Regex Version of the strip() Method
+"""
+Write a function that takes a string and does the same thing as the strip() string method. 
+
+If no other arguments are passed other than the string to strip, 
+then whitespace characters will be removed from the beginning and end of the string. 
+Otherwise, the characters specified in the second argument to the function will be removed from the string.
+"""
+
+import re
+
+def stripRegex(string, removalChr = ' '):
+
+    print('\n', string)
+    print(f"removalChr : '{removalChr}'")
+
+    if removalChr == ' ':
+        sRegex = re.compile(r'\S+.*\S+')
+        mo = sRegex.findall(string)
+        print(f"Result : \n'{mo[0]}'")    
+    else:
+        # sRegex = re.compile(r'[(%s)]'%{removalChr})    
+        sRegex = re.compile(fr'[{removalChr}]')    
+        mo = sRegex.sub('', text)
+        print(f"Result : \n'{mo}'")
+
+
+text = "  hello, my name is lisa.   "
+
+stripRegex(text)
+stripRegex(text, 'e')
